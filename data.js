0300f097e0fb866140561df15f4d6b99eeb39926
gnanam17 <156506196+gnanam17@users.noreply.github.com>
2024-01-13 14:16:50 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+    darkModeToggle.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+    });
+});
